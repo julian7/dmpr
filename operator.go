d09ac78ef9b8d58da8cd82d2eab147df85c48cf2
Balazs Nagy <julsevern@gmail.com>
2019-08-16 20:00:22 +0200

    better detection for subgroups

--- operator.go
+++ operator.go
@@ -85,14 +85,19 @@ func (op *NOT) Where(truthy bool) string {
 	return op.Operator.Where(!truthy)
 }
 
+type Grouper interface {
+	Add(...Operator)
+}
+
 // GroupOperator is an iternal data structure for an operator with multiple sub-operators
 type GroupOperator struct {
 	items []Operator
+	op    string
 }
 
 // NewGroupOp returns a new group of operators
-func NewGroupOp(items ...Operator) GroupOperator {
-	return GroupOperator{items: items}
+func NewGroupOp(op string, items ...Operator) *GroupOperator {
+	return &GroupOperator{items: items, op: op}
 }
 
 // Add adds more operatorn to an existing GroupOperator
@@ -111,19 +116,40 @@ func (op *GroupOperator) Value() map[string]interface{} {
 	return values
 }
 
+// Where is a helper function for implementer structs to provide all where clauses
+func (op *GroupOperator) Where(truthy bool) string {
+	if len(op.items) == 1 {
+		return op.items[0].Where(truthy)
+	}
+	whereClauses := make([]string, 0, len(op.items))
+	for _, item := range op.items {
+		where := item.Where(true)
+		if _, ok := item.(Grouper); ok {
+			where = fmt.Sprintf("(%s)", item.Where(true))
+		}
+		whereClauses = append(whereClauses, where)
+	}
+	ret := strings.Join(whereClauses, fmt.Sprintf(" %s ", op.op))
+	if !truthy {
+		ret = fmt.Sprintf("NOT (%s)", ret)
+	}
+	return ret
+}
+
 // AND is an operator struct, with AND relation between each item
 type AND struct {
-	GroupOperator
+	*GroupOperator
 }
 
 // And returns a new AND operator with initial values
 func And(ops ...Operator) *AND {
-	op := &AND{GroupOperator: NewGroupOp([]Operator{}...)}
-	return op.And(ops...)
+	op := &AND{GroupOperator: NewGroupOp("AND", []Operator{}...)}
+	op.Add(ops...)
+	return op
 }
 
-// And adds more items into the AND operator
-func (op *AND) And(ops ...Operator) *AND {
+// Add adds more items into the AND operator
+func (op *AND) Add(ops ...Operator) {
 	for _, item := range ops {
 		if andop, ok := item.(*AND); ok {
 			op.GroupOperator.Add(andop.items...)
@@ -131,17 +157,49 @@ func (op *AND) And(ops ...Operator) *AND {
 			op.GroupOperator.Add(item)
 		}
 	}
+}
+
+// And is an alias to Add, and it returns itself for chaining
+func (op *AND) And(ops ...Operator) *AND {
+	op.Add(ops...)
 	return op
 }
 
 // Where returns the where clause of all sub-operators stringed together into an AND clause
 func (op *AND) Where(truthy bool) string {
-	if len(op.items) == 1 {
-		return op.items[0].Where(truthy)
+	return op.GroupOperator.Where(truthy)
+}
+
+// OR is an operator struct, with OR relation between each item
+type OR struct {
+	*GroupOperator
+}
+
+// And returns a new OR operator with initial values
+func Or(ops ...Operator) *OR {
+	op := &OR{GroupOperator: NewGroupOp("OR", []Operator{}...)}
+	op.Add(ops...)
+	return op
+}
+
+// Add adds more items into the OR operator
+func (op *OR) Add(ops ...Operator) {
+	for _, item := range ops {
+		if andop, ok := item.(*OR); ok {
+			op.GroupOperator.Add(andop.items...)
+		} else {
+			op.GroupOperator.Add(item)
+		}
 	}
-	whereClauses := make([]string, 0, len(op.items))
-	for _, item := range op.items {
-		whereClauses = append(whereClauses, item.Where(true))
-	}
-	return map[bool]string{true: "", false: "NOT "}[truthy] + "(" + strings.Join(whereClauses, " AND ") + ")"
+}
+
+// Or is an alias to Add, and it returns itself for chaining
+func (op *OR) Or(ops ...Operator) *OR {
+	op.Add(ops...)
+	return op
+}
+
+// Where returns the where clause of all sub-operators stringed together into an OR clause
+func (op *OR) Where(truthy bool) string {
+	return op.GroupOperator.Where(truthy)
 }
--- operator_test.go
+++ operator_test.go
@@ -158,17 +158,17 @@ func TestAnd(t *testing.T) {
 		{
 			name: "single item",
 			op:   []Operator{Eq("one", "two")},
-			want: &AND{GroupOperator: GroupOperator{items: []Operator{Eq("one", "two")}}},
+			want: &AND{GroupOperator: &GroupOperator{op: "AND", items: []Operator{Eq("one", "two")}}},
 		},
 		{
 			name: "swallows sub-and item",
 			op:   []Operator{And(Eq("one", "two"))},
-			want: &AND{GroupOperator: GroupOperator{items: []Operator{Eq("one", "two")}}},
+			want: &AND{GroupOperator: &GroupOperator{op: "AND", items: []Operator{Eq("one", "two")}}},
 		},
 		{
 			name: "multiple single items",
 			op:   []Operator{And(Eq("one", "two"), Eq("three", "four"))},
-			want: &AND{GroupOperator: GroupOperator{items: []Operator{
+			want: &AND{GroupOperator: &GroupOperator{op: "AND", items: []Operator{
 				Eq("one", "two"),
 				Eq("three", "four"),
 			}}},
@@ -176,7 +176,7 @@ func TestAnd(t *testing.T) {
 		{
 			name: "multiple group items",
 			op:   []Operator{And(Eq("one", "two"), And(Eq("three", "four"), Eq("five", "six")))},
-			want: &AND{GroupOperator: GroupOperator{items: []Operator{
+			want: &AND{GroupOperator: &GroupOperator{op: "AND", items: []Operator{
 				Eq("one", "two"),
 				Eq("three", "four"),
 				Eq("five", "six"),
@@ -187,6 +187,53 @@ func TestAnd(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := And(tt.op...); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("And() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGroupOperator_Where(t *testing.T) {
+	tests := []struct {
+		name   string
+		op     *GroupOperator
+		truthy bool
+		want   string
+	}{
+		{
+			name:   "single item",
+			op:     NewGroupOp("+", Eq("one", "two")),
+			truthy: true,
+			want:   "one = :one",
+		},
+		{
+			name:   "negated single item",
+			op:     NewGroupOp("+", Eq("one", "two")),
+			truthy: false,
+			want:   "one <> :one",
+		},
+		{
+			name:   "multiple items",
+			op:     NewGroupOp("+", Eq("one", "two"), Eq("three", "four"), Eq("five", "six")),
+			truthy: true,
+			want:   "one = :one + three = :three + five = :five",
+		},
+		{
+			name:   "negate multiple items",
+			op:     NewGroupOp("+", Eq("one", "two"), Eq("three", "four"), Eq("five", "six")),
+			truthy: false,
+			want:   "NOT (one = :one + three = :three + five = :five)",
+		},
+		{
+			name:   "negate multiple items 2",
+			op:     NewGroupOp("+", Eq("one", "two"), NewGroupOp("-", Eq("three", "four"), Eq("five", "six"))),
+			truthy: false,
+			want:   "NOT (one = :one + (three = :three - five = :five))",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.op.Where(tt.truthy); got != tt.want {
+				t.Errorf("AND.Where() = %v, want %v", got, tt.want)
 			}
 		})
 	}
@@ -203,7 +250,7 @@ func TestAND_And(t *testing.T) {
 			name:  "add single item",
 			op:    Eq("one", "two"),
 			other: []Operator{Eq("three", "four")},
-			want: &AND{GroupOperator: GroupOperator{items: []Operator{
+			want: &AND{GroupOperator: &GroupOperator{op: "AND", items: []Operator{
 				Eq("one", "two"),
 				Eq("three", "four"),
 			}}},
@@ -212,7 +259,7 @@ func TestAND_And(t *testing.T) {
 			name:  "add multiple items",
 			op:    Eq("one", "two"),
 			other: []Operator{Eq("three", "four"), Eq("five", "six")},
-			want: &AND{GroupOperator: GroupOperator{items: []Operator{
+			want: &AND{GroupOperator: &GroupOperator{op: "AND", items: []Operator{
 				Eq("one", "two"),
 				Eq("three", "four"),
 				Eq("five", "six"),
@@ -222,7 +269,7 @@ func TestAND_And(t *testing.T) {
 			name:  "add sub-and",
 			op:    Eq("one", "two"),
 			other: []Operator{And(Eq("three", "four"), Eq("five", "six"))},
-			want: &AND{GroupOperator: GroupOperator{items: []Operator{
+			want: &AND{GroupOperator: &GroupOperator{op: "AND", items: []Operator{
 				Eq("one", "two"),
 				Eq("three", "four"),
 				Eq("five", "six"),
@@ -232,7 +279,7 @@ func TestAND_And(t *testing.T) {
 			name:  "multiple .And()s",
 			op:    Eq("one", "two"),
 			other: []Operator{And(Eq("three", "four")).And(Eq("five", "six"))},
-			want: &AND{GroupOperator: GroupOperator{items: []Operator{
+			want: &AND{GroupOperator: &GroupOperator{op: "AND", items: []Operator{
 				Eq("one", "two"),
 				Eq("three", "four"),
 				Eq("five", "six"),
@@ -248,42 +295,57 @@ func TestAND_And(t *testing.T) {
 	}
 }
 
-func TestAND_Where(t *testing.T) {
+func TestOR_Or(t *testing.T) {
 	tests := []struct {
-		name   string
-		op     *AND
-		truthy bool
-		want   string
+		name  string
+		op    Operator
+		other []Operator
+		want  *OR
 	}{
 		{
-			name:   "single item",
-			op:     And(Eq("one", "two")),
-			truthy: true,
-			want:   "one = :one",
+			name:  "add single item",
+			op:    Eq("one", "two"),
+			other: []Operator{Eq("three", "four")},
+			want: &OR{GroupOperator: &GroupOperator{op: "OR", items: []Operator{
+				Eq("one", "two"),
+				Eq("three", "four"),
+			}}},
 		},
 		{
-			name:   "negated single item",
-			op:     And(Eq("one", "two")),
-			truthy: false,
-			want:   "one <> :one",
+			name:  "add multiple items",
+			op:    Eq("one", "two"),
+			other: []Operator{Eq("three", "four"), Eq("five", "six")},
+			want: &OR{GroupOperator: &GroupOperator{op: "OR", items: []Operator{
+				Eq("one", "two"),
+				Eq("three", "four"),
+				Eq("five", "six"),
+			}}},
 		},
 		{
-			name:   "multiple items",
-			op:     And(Eq("one", "two"), Eq("three", "four"), Eq("five", "six")),
-			truthy: true,
-			want:   "(one = :one AND three = :three AND five = :five)",
+			name:  "add sub-and",
+			op:    Eq("one", "two"),
+			other: []Operator{Or(Eq("three", "four"), Eq("five", "six"))},
+			want: &OR{GroupOperator: &GroupOperator{op: "OR", items: []Operator{
+				Eq("one", "two"),
+				Eq("three", "four"),
+				Eq("five", "six"),
+			}}},
 		},
 		{
-			name:   "negate multiple items",
-			op:     And(Eq("one", "two"), Eq("three", "four"), Eq("five", "six")),
-			truthy: false,
-			want:   "NOT (one = :one AND three = :three AND five = :five)",
+			name:  "multiple .Or()s",
+			op:    Eq("one", "two"),
+			other: []Operator{Or(Eq("three", "four")).Or(Eq("five", "six"))},
+			want: &OR{GroupOperator: &GroupOperator{op: "OR", items: []Operator{
+				Eq("one", "two"),
+				Eq("three", "four"),
+				Eq("five", "six"),
+			}}},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.op.Where(tt.truthy); got != tt.want {
-				t.Errorf("AND.Where() = %v, want %v", got, tt.want)
+			if got := Or(tt.op).Or(tt.other...); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("OR.Or() = %#v, want %#v", got, tt.want)
 			}
 		})
 	}
